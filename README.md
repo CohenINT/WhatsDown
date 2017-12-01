df905a5ad68131347475b11c96eb77e5be1af05d
Muthu Kumar <muthukumar@thefeathers.in>
2017-12-01 23:24:19 +0530

    Bug fixes

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ An experiment in backing up your WhatsApp chats using the WhatsApp Web interface
 
 ## Version
 
+`v. 0.1.0` : Bug fixes
+
 `v. 0.0.1` : Initial commit. When the extension icon is clicked, all the messages in current chat will be logged in console.
--- content.js
+++ content.js
@@ -1,9 +1,18 @@
 function injectthis() {
-	messages = document.getElementsByClassName("bubble")
+	var messages = document.getElementsByClassName("bubble")
 	for(var i=0; i<messages.length; i++){
-		var messageHeader = (messages[i].dataset['prePlainText'])//.split("]")
-		var messageContent = messagecontent = /( -->)(.+)?(<!-- \/react-text)/g.exec(messages[i].innerHTML)
-		var content = messageHeader + "\n" + messagecontent[2]
+		headerPattern = /[^[\]][\sa-zA-Z:,/\d]*/g // Extracts Time and Name separately (use later)
+		var messageHeader = messages[i].dataset['prePlainText']
+		messageHeader = messageHeader ? messageHeader : ''
+
+		var messageContent = ''
+		regexp = /-->[^<][\s\S]+?<!--/g // Takes the innerHTML from markup between (and including) --> and <!--
+		while ((match = regexp.exec(messages[i].innerHTML)) != null) { // Gets all matches and adds them together
+			var addTo = (/[^(-->)].*[^(<!--)]/g.exec(match[0]))[0] // Removes --> and <!--
+			messageContent = messageContent + addTo
+		}
+
+		var content =  messageHeader + "\n" + (messageContent ? messageContent : '<emoji skipped>')
 		console.log(content)
 	}
 }
